27170c36afe3f13321192f8ec8ca6d8732e27314
ryoua <1794970183@qq.com>
2020-08-30 20:08:26 +0800

    添加注释

--- core/src/main/java/com/dq/core/timer/Timer.java
+++ core/src/main/java/com/dq/core/timer/Timer.java
@@ -5,9 +5,20 @@ package com.dq.core.timer;
  * * @Date: 2020/8/30
  **/
 public interface Timer extends Runnable {
+    /**
+     * 扫描器的初始化方法
+     */
     void init();
 
+    /**
+     * 扫描器的扫描方法
+     * 1.while死循环
+     * 2.wait/notify
+     */
     void scan();
 
+    /**
+     * 扫描器的销毁方法
+     */
     void destroy();
 }
